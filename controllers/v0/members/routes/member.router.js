2eda38e0163853489adea343e33b36063fea980c
kinderlars <l.kinder187@gmail.com>
2021-01-21 17:13:21 +0100

    Removed try catch to increase performance

--- controllers/v0/members/routes/member.router.js
+++ controllers/v0/members/routes/member.router.js
@@ -21,28 +21,24 @@ router.get('/test', async (req, res, next) => {
 
 /* GET all users, if exists */
 router.get('/',async (req,res) => {
-  try{
-    let queryParam = req.query;
+  let queryParam = req.query;
 
-    if(members.length === 0){
-      return res.status(400).send({message:"No data found!"});
-    }
-
-    // Checking for params === {} will not work
-    if( Object.keys(queryParam).length === 0 ){
-     return res.status(200).json(members).send();
-    }
-
-    const member = findUserByParam(queryParam);
-    if(typeof member === 'undefined'){
-      return res.status(400).send({message:`No entry found that matches the query parameter ${Object.keys(queryParam)}`})
-    }
-    console.log(member);
-    res.status(200).json(member).send();
-
-  }catch(e){
-    console.error(e.message)
+  if(members.length === 0){
+    return res.status(400).send({message:"No data found!"});
   }
+
+  // Checking for params === {} will not work
+  if( Object.keys(queryParam).length === 0 ){
+    return res.status(200).json(members).send();
+  }
+
+  const member = findUserByParam(queryParam);
+  if(typeof member === 'undefined'){
+    return res.status(400).send({message:`No entry found that matches the query parameter ${Object.keys(queryParam)}`})
+  }
+  console.log(member);
+  res.status(200).json(member).send();
+
 });
 
 router.get('/:id',async (req,res) => {
@@ -70,26 +66,23 @@ router.get('/',async (req,res) => {
 
 /* POST new user, and add uuid as id */
 router.post('/',async (req,res) => {
-  try{
-    if(Object.keys(req.body).length === 0){
-      return res.status(400).send({message: "Empty payload"})
-    }
-
-    const id = uuid.v4()
-
-    const member = {
-      id: id,
-      firstname: req.body.firstname,
-      surname: req.body.surname,
-      email: req.body.email,
-      status: req.body.status
-    }
-
-    members.push(member)
-    res.status(200).send(member)
-  }catch (e){
-    console.error(e.message);
+  if(Object.keys(req.body).length === 0){
+    return res.status(400).send({message: "Empty payload"})
   }
+
+  const id = uuid.v4()
+
+  const member = {
+    id: id,
+    firstname: req.body.firstname,
+    surname: req.body.surname,
+    email: req.body.email,
+    status: req.body.status
+  }
+
+  members.push(member)
+  res.status(200).send(member)
+
 })
 
 function findUserById(id){
--- controllers/v0/products/routes/products.router.js
+++ controllers/v0/products/routes/products.router.js
@@ -17,46 +17,38 @@ router.use(bodyParser.json());
 let products = [];
 
 router.get('/', async (req, res) => {
-  try{
-    // TODO: This is causing me problems, as I need the output of the getProducts function
-    const results = await getProducts();
-    results.then(products.push(results))
-    console.log(products)
-    
-    if(products.length){
-      return res.status(400).send({message: "No data was found in table"})
-    }
-    res.status(200).send(`I succeeded ${JSON.stringify(products)}`)
-    // if(products.length===0){
-    //   return res.status(400).send("No products found!")
-    // }
-    //res.status(200).send(JSON.stringify(products))
-  }catch (e){
-    console.error(e.message)
+  // TODO: This is causing me problems, as I need the output of the getProducts function
+  const results = await getProducts();
+  products.push(results)
+  console.log(products)
+  
+  if(products.length){
+    return res.status(400).send({message: "No data was found in table"})
   }
+  res.status(200).send(`I succeeded ${JSON.stringify(products)}`)
+  // if(products.length===0){
+  //   return res.status(400).send("No products found!")
+  // }
+  //res.status(200).send(JSON.stringify(products))
 })
 
 router.post('/', async (req,res) => {
-  try{
-    if(Object.keys(req.body).length === 0) {
-      return res.status(400).send({message: "Empty payload"})
-    }
-
-    const id = uuid.v4()
-
-    const product = {
-      id: id,
-      name: req.body.name,
-      description: req.body.description,
-      vendor: req.body.vendor
-    }
-
-    products.push(product)
-    res.status(200).send(product)
-
-  }catch (e) {
-    console.error(e.message)
+  if(Object.keys(req.body).length === 0) {
+    return res.status(400).send({message: "Empty payload"})
   }
+
+  const id = uuid.v4()
+
+  const product = {
+    id: id,
+    name: req.body.name,
+    description: req.body.description,
+    vendor: req.body.vendor
+  }
+
+  products.push(product)
+  res.status(200).send(product)
+
 })
 
 export const ProductRouter = router;
